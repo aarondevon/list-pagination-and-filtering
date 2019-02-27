edaa0629d05debb4748808a6e3afc247292c34c6
Aaron Sawyer <me@aarondevon.com>
2019-02-27 11:09:59 -0600

    update comments, display students on correct page, add active class to correct page link

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
+// Create div element
+const divPages = document.createElement('div');
+
+// Create ul element
+let ul = document.createElement('ul');
+
+// Add class names
+divPages.className = 'pagination';
+ul.className = 'pages';
+
 // Get needed elements
 const studentList = document.querySelector('.student-list').getElementsByTagName('li');
 const page = document.querySelector('.page');
+const liList = ul.getElementsByTagName('li');
+
+// Append ul and divPages
+divPages.appendChild(ul);
+page.appendChild(divPages);
+
 // Set max number of students on each page
 const maxItemsPerPage = 10;
 
-// Show Page
+// Get number of pages needed
+const numOfPages = studentList.length / 10;
+
+// Return student array for page
 const showPage = (list, page) => {
 	const studentsOnPage = [];
 	for (let i = 0; i < list.length; i++)
@@ -14,35 +33,55 @@ const showPage = (list, page) => {
 		return studentsOnPage;
 }
 
-
-
 // Append page links
 const appendPageLinks = (list) => {
-	// Get number of pages needed
-	const numOfPages = list.length / 10;
-	// Create div element
-	const divPages = document.createElement('div');
-	// Create ul element
-	const ul = document.createElement('ul');
-
-	// Add class names
-	divPages.className = 'pagination';
-	ul.className = 'pages';
-
-	// Append ul and divPages
-	divPages.appendChild(ul);
-	page.appendChild(divPages);
-	
+	let pageNumber;		
 	// Create buttons
 	for ( let i = 0; i < numOfPages; i++) {
 		let li = document.createElement('li');
-		let a = document.createElement('a');	
+		let a = document.createElement('a');
 		a.setAttribute('href', "#");
 		a.textContent = i + 1;
 		li.appendChild(a);
     ul.appendChild(li);
 	}
-	const a = ul.query
-	console.log(a);
-	
+	// Hide All Students
+	for (let i = 0; i < studentList.length; i ++) {
+		studentList[i].style.display = 'none';
+	}
+	// Show students for page 1	
+	for (let i = 0; i < showPage(studentList, 1).length; i ++) {
+		showPage(studentList, 1)[i].style.display = '';
+	}
+	// Add Active class to first page
+	liList[0].firstElementChild.className = 'active';
+	// Add event listener to to page links
+	for ( let i = 0; i < numOfPages; i++) {
+		liList[i].addEventListener('click', (event) => {
+		
+			if (event.target.tagName === 'A') {
+				// Get page number
+				pageNumber = parseInt(event.target.textContent);
+				// Remove active class on other page links when page link is clicked
+				for (let i = 0; i < numOfPages; i++) {
+					if (liList[i].firstElementChild.classList.contains('active')) {
+						liList[i].firstElementChild.classList.remove('active');
+					}
+				}
+				// Add active class to target page link
+				event.target.className = 'active';
+				// Hide student list again
+				for (let i = 0; i < studentList.length; i ++) {
+					studentList[i].style.display = 'none';
+				}
+				// Display students for correct page link clicked
+				for (let i = 0; i < showPage(studentList, pageNumber).length; i ++) {
+					showPage(studentList, pageNumber)[i].style.display = '';
+				}
+			}
+		})
+	}
 };
+
+appendPageLinks(studentList);
+
